20e79a1c7bb996e4a5ce8b611c685c9b98a4d2ff
gmarcinek <grzegorz.marcinek@gmail.com>
2025-04-25 16:05:08 +0200

    initial commit

--- script.js
+++ script.js
@@ -5,26 +5,25 @@ document.addEventListener("DOMContentLoaded", () => {
       e.preventDefault();
   
       // Pobranie wartości z formularza
-      const L_p1 = parseFloat(document.getElementById("L_p1").value);
-      const L_p2 = parseFloat(document.getElementById("L_p2").value);
-      const L_p3 = parseFloat(document.getElementById("L_p3").value);
-      const C_joint = parseFloat(document.getElementById("C_joint").value);
+      const L_p2 = parseFloat(document.getElementById("L_p2").value);  // długość LP2
+      const L_p3 = parseFloat(document.getElementById("L_p3").value);  // długość LP3
+      const C_joint = parseFloat(document.getElementById("C_joint").value);  // obwód stawu LP2/LP3
       const weight = parseFloat(document.getElementById("weight").value);
   
-      // Obliczanie całkowitej długości palca (sumowanie długości poszczególnych paliczków)
-      const L_total = L_p1 + L_p2 + L_p3;
+      // Obliczenie całkowitej długości dźwigni przy half crimpie
+      const L_lever = L_p2 + L_p3;
   
-      // Obliczenie BGI (Biomechanical Grip Index) na podstawie długości palca i obwodu stawu
-      const BGI = (L_total ** 2) / C_joint;
+      // Obliczenie BGI (Biomechanical Grip Index) dla half crimp
+      const BGI = (L_lever ** 2) / C_joint;
   
       // Korekta współczynnika siły: Zmiana - krótsze paliczki = wyższa siła
-      const k = 1 / (L_total / C_joint);  // Przyjmujemy, że dźwignia jest odwrotnie proporcjonalna do L_total
+      const k = 1 / (L_lever / C_joint);  // Przyjmujemy, że dźwignia jest odwrotnie proporcjonalna do długości dźwigni
   
       // Siła potencjalna (przyjmujemy różne wartości dla osób trenujących i nietrenujących)
       const maxPotentialForce = k * weight;
   
-      // Zakładając, że osoba trenująca ma siłę większą o ~70% w porównaniu do osoby nietrenującej:
-      const maxTrained = maxPotentialForce * 1.7;     // Trenujący (wyższa siła)
+      // Zakładając, że osoba trenująca ma siłę większą o ~2.5 razy w porównaniu do osoby nietrenującej:
+      const maxTrained = maxPotentialForce * 2.5;     // Trenujący (wyższa siła)
       const maxUntrained = maxPotentialForce * 0.8;   // Nietrenujący (niższa siła)
   
       // Wyświetlanie wyników na stronie
